13864ae31ee9b1c6b22e585468dacb405566cce4
Joshua Lai <79826865+jl1218@users.noreply.github.com>
2021-03-09 14:02:11 +0800

    Update form.js

--- form.js
+++ form.js
@@ -1,12 +1,12 @@
 function submit() {
-    var name = document.getElementById("fname").value + " " + document.getElementById("lname").value;
-    var email = document.getElementById("email").value;
+    var name = $("#fname").value + " " + $("#lname").value;
+    var email = $("#email").value;
     var reason = $("input[name=reason]:checked", "#reason").val();
-    var comment = document.getElementById("comment").value;
+    var comment = $("#comment").value;
     var text = `Name: ${name}<br>Email: ${email}<br>Reason of contact: ${reason}<br>Comment: ${comment}`;
-    document.getElementById("form").style.display = "none";
-    document.getElementById("result").style.display = "block";
-    document.getElementById("result").innerHTML = text;
+    $("#form").style.display = "none";
+    $("#result").style.display = "block";
+    $("#result").innerHTML = text;
 }
 $("textarea").keyup(function () {
     if (this.value.length > $(this).attr("maxlength")) {
