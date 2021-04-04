8cb507633dda971d2b230aae8f8e55d9cfa05b79
Joshua Lai <79826865+jl1218@users.noreply.github.com>
2021-04-04 10:51:37 +0800

    Update form.js

--- form.js
+++ form.js
@@ -1,18 +1,30 @@
+var md = new Remarkable({
+	html: true,
+	xhtmlOut: false,
+	breaks: false,
+	linkify: true,
+	typographer: false,
+	quotes: "“”‘’"
+});
 function submit() {
-    var name = $("#fname").value + " " + $("#lname").value;
-    var email = $("#email").value;
-    var reason = $("input[name=reason]:checked", "#reason").val();
-    var comment = $("#comment").value;
-    var text = `Name: ${name}<br>Email: ${email}<br>Reason of contact: ${reason}<br>Comment: ${comment}`;
-    $("#form").style.display = "none";
-    $("#result").style.display = "block";
-    $("#result").innerHTML = text;
+	var name =
+		document.getElementById("fname").value +
+		" " +
+		document.getElementById("lname").value;
+	var email = document.getElementById("email").value;
+	var reason = $("input[name=reason]:checked", "#reason").val();
+	var notparsedcomment = document.getElementById("comment").value;
+	var comment = md.render(notparsedcomment);
+	var text = `Name: ${name}<br>Email: ${email}<br>Reason of contact: ${reason}<br>Comment: ${comment}`;
+	document.getElementById("form").style.display = "none";
+	document.getElementById("result").style.display = "block";
+	document.getElementById("result").innerHTML = text;
 }
 $("textarea").keyup(function () {
-    if (this.value.length > $(this).attr("maxlength")) {
-        return false;
-    }
-    $(this)
-        .next()
-        .html(this.value.length + "/" + $(this).attr("maxlength"));
+	if (this.value.length > $(this).attr("maxlength")) {
+		return false;
+	}
+	$(this)
+		.next()
+		.html(this.value.length + "/" + $(this).attr("maxlength"));
 });
